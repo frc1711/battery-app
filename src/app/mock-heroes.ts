169da82d136b1ee264068206629ad3a4f288c3e2
sedezee <32148160+sedezee@users.noreply.github.com>
2018-02-03 16:58:24 -0500

    Changed names and constant batteries

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,5 @@
 import { Battery } from './batteries';
 
 export const BATTERIES: Battery[] = [
-  { id: 11, name: 'Mr. Nice', charge:10 },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 0, name: Fred }
 ];
