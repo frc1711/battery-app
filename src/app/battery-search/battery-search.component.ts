7f1e38c858d25b4109f2393336e6817f9093df8d
sedezee <32148160+sedezee@users.noreply.github.com>
2018-02-03 14:33:01 -0500

    Changed names

--- src/app/battery-search/battery-search.component.ts
+++ src/app/battery-search/battery-search.component.ts
@@ -8,19 +8,19 @@ import {
    debounceTime, distinctUntilChanged, switchMap
  } from 'rxjs/operators';
 
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
+import { Battery } from '../battery';
+import { BatteryService } from '../battery.service';
 
 @Component({
-  selector: 'app-hero-search',
-  templateUrl: './hero-search.component.html',
-  styleUrls: [ './hero-search.component.css' ]
+  selector: 'app-battery-search',
+  templateUrl: './battery-search.component.html',
+  styleUrls: [ './battery-search.component.css' ]
 })
-export class HeroSearchComponent implements OnInit {
-  heroes$: Observable<Hero[]>;
+export class BatterySearchComponent implements OnInit {
+  batteries$: Observable<Battery[]>;
   private searchTerms = new Subject<string>();
 
-  constructor(private heroService: HeroService) {}
+  constructor(private batteryService: BatteryService) {}
 
   // Push a search term into the observable stream.
   search(term: string): void {
@@ -28,7 +28,7 @@ export class HeroSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.heroes$ = this.searchTerms.pipe(
+    this.batteries$ = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
       debounceTime(300),
 
@@ -36,7 +36,7 @@ export class HeroSearchComponent implements OnInit {
       distinctUntilChanged(),
 
       // switch to new search observable each time the term changes
-      switchMap((term: string) => this.heroService.searchHeroes(term)),
+      switchMap((term: string) => this.batteryService.searchBatteries(term)),
     );
   }
 }
