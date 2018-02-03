5e431f9cd7a0aafaf20c7d364aff3b4a19532c91
sedezee <32148160+sedezee@users.noreply.github.com>
2018-02-03 16:50:26 -0500

    Changed names and updated const batteries

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,19 +1,10 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
- 
+
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const heroes = [
-      { id: 11, name: 'Fat Stacks' },
- 	  { id: 12, name: 'Bagel' },
-	  { id: 13, name: 'Sceince' },
-	  { id: 14, name: 'Banannie' },
-	  { id: 15, name: 'Death Ray' },
-	  { id: 16, name: 'Fred' },
-  	  { id: 17, name: 'Captain Hammer' },
-	  { id: 18, name: 'Dr. Horrible' },
-	  { id: 19, name: 'Peytong' },
-	  { id: 20, name: 'Sharknado' }
+    const batteries = [
+     {id: 0, name: "Fred"}
     ];
-    return {heroes};
+    return {batteries};
   }
 }
