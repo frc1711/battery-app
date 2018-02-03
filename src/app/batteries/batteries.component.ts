5bd78f6aeaa93f72be2b01c40167a7f65e44d18f
sedezee <32148160+sedezee@users.noreply.github.com>
2018-02-03 16:38:43 -0500

    Changed names

---
+++ src/app/batteries/batteries.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Battery } from '../battery';
+import { BatteryService } from '../battery.service';
+
+@Component({
+  selector: 'app-batteries',
+  templateUrl: './batteries.component.html',
+  styleUrls: ['./batteries.component.css']
+})
+export class BatteriesComponent implements OnInit {
+  batteries: Battery[];
+
+  constructor(private batteryService: BatteryService) { }
+
+  ngOnInit() {
+    this.getBatteries();
+  }
+
+  getBatteries(): void {
+    this.batteryService.getBatteries()
+    .subscribe(batteries => this.batteries = batteries);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.batteryService.addBattery({ name } as Battery)
+      .subscribe(battery => {
+        this.batteries.push(battery);
+      });
+  }
+
+  delete(battery: Battery): void {
+    this.batteries = this.batteries.filter(h => h !== battery);
+    this.batteryService.deleteBattery(battery).subscribe();
+  }
+
+}
