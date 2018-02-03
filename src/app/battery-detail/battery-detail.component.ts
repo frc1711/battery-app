beb75635878abffe18cee1ab057cd3b6134b8433
sedezee <32148160+sedezee@users.noreply.github.com>
2018-02-03 14:29:33 -0500

    Changed names

---
+++ src/app/battery-detail/battery-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Battery }         from '../battery';
+import { BatteryService }  from '../battery.service';
+
+@Component({
+  selector: 'app-battery-detail',
+  templateUrl: './battery-detail.component.html',
+  styleUrls: [ './battery-detail.component.css' ]
+})
+export class BatteryDetailComponent implements OnInit {
+  @Input() battery: Battery;
+
+  constructor(
+    private route: ActivatedRoute,
+    private batteryService: BatteryService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getBattery();
+  }
+
+  getBattery(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.batteryService.getBattery(id)
+      .subscribe(battery => this.battery = battery);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+ save(): void {
+    this.batteryService.updateBattery(this.battery)
+      .subscribe(() => this.goBack());
+  }
+}
