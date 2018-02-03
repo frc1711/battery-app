5237065c9c8c4d6ccf70d89747b91c15610d3a96
Rayna Archibald <rayna.archibald@gmail.com>
2018-02-03 10:37:16 -0500

    Initital commit.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Fat Stacks' },
+  { id: 12, name: 'Bagel' },
+  { id: 13, name: 'Sceince' },
+  { id: 14, name: 'Banannie' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
