e9172a17f0d3e55123f8524e1b17cecd114ebe52
sedezee <32148160+sedezee@users.noreply.github.com>
2018-02-03 16:44:28 -0500

    Changed names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { BatteriesComponent }      from './batteries/batteries.component';
+import { BatteryDetailComponent }  from './battery-detail/battery-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'detail/:id', component: BatteryDetailComponent },
+  { path: 'batteries', component: BatteriesComponent }
 ];
 
 @NgModule({
