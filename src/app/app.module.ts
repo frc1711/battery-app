fcae68b56a46a1ef480b3fa673a18560e2aebd4d
sedezee <32148160+sedezee@users.noreply.github.com>
2018-02-03 16:46:30 -0500

    Changed names

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
- 
+
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
- 
+
 import { AppRoutingModule }     from './app-routing.module';
- 
+
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroSearchComponent }  from './hero-search/hero-search.component';
-import { HeroService }          from './hero.service';
+import { BatteryDetailComponent }  from './battery-detail/battery-detail.component';
+import { BatteriesComponent }      from './batteries/batteries.component';
+import { BatterySearchComponent }  from './battery-search/battery-search.component';
+import { BatteryService }          from './battery.service';
 import { MessageService }       from './message.service';
 import { MessagesComponent }    from './messages/messages.component';
 
@@ -24,22 +24,22 @@ import { MessagesComponent }    from './messages/messages.component';
     AppRoutingModule,
     HttpClientModule,
 
-// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-// and returns simulated server responses.
-// Remove it when a real server is ready to receive requests.
-HttpClientInMemoryWebApiModule.forRoot(
-  InMemoryDataService, { dataEncapsulation: false }
-)
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
+    BatteriesComponent,
+    BatteryDetailComponent,
     MessagesComponent,
-    HeroSearchComponent
+    BatterySearchComponent
   ],
-  providers: [ HeroService, MessageService ],
+  providers: [ BatteryService, MessageService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
